5c28fb9266261f87a5d9732916cf74f38611c0f1
Nova <47910472+novathefox@users.noreply.github.com>
2024-05-14 20:11:52 +1000

    More NovaCast work

--- libraries/novacast.js
+++ libraries/novacast.js
@@ -1,18 +1,20 @@
-import './ht2canvas.js'
 window.novacast = {
-    test: function(){
-        html2canvas(document.body).then(canvas => {
-            let canvasUrl = canvas.toDataURL();
-            console.log(canvasUrl);
-            let createEl = document.createElement('a');
-            createEl.href = canvasUrl;
-        
-            // This is the name of our downloaded file
-            createEl.download = "download-this-canvas";
-        
-            // Click the download button, causing a download, and then remove it
-            createEl.click();
-            createEl.remove();
-        });
+    test: function () {
+        alert()
+        domtoimage.toPng(document.body.parentElement)
+            .then(function (dataUrl) {
+                let createEl = document.createElement('a');
+                createEl.href = dataUrl;
+
+                // This is the name of our downloaded file
+                createEl.download = "download-this-canvas";
+
+                // Click the download button, causing a download, and then remove it
+                createEl.click();
+                createEl.remove();
+            })
+            .catch(function (error) {
+                console.error('oops, something went wrong!', error);
+            });
     }
 }
